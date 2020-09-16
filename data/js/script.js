ceaf282c56cb6a28a1ec3f7c7ce1807357d72cbc
NEXTJun <next0309@gmail.com>
2020-09-17 02:04:05 +0800

    Modify html typesetting

--- data/js/script.js
+++ data/js/script.js
@@ -1,40 +1,42 @@
+const all_money = 100000000000;
+
 var app = new Vue({
   el: "#app",
   data: {
     itemList: [
       {
-        id: "1",
+        id: 1,
         name: "萊莎的鍊金工房 2",
         imgUrl:
           "img/f8b5b152d5efcbbd42141a9c6e19e5u5.jpg",
-        price: "1990",
+        price: 1990,
         count: 0,
         amount: 99
       },
       {
-        id: "2",
+        id: 2,
         name: "薩爾達無雙 災厄啟示錄",
         imgUrl:
           "img/a31970bdabb8f2d0678c700e2119thn5.jpg",
-        price: "2390",
+        price: 2390,
         count: 0,
         amount: 99
       },
       {
-        id: "3",
+        id: 3,
         name: "十三機兵防衛圈",
         imgUrl:
           "img/f35303289e76f0062dcbb16590170tu5.jpg",
-        price: "1790",
+        price: 1790,
         count: 0,
         amount: 99
       },
       {
-        id: "4",
+        id: 4,
         name: "惡靈古堡3",
         imgUrl:
           "img/b4162ce3568e9a669f182ad471175io5.jpg",
-        price: "1790",
+        price: 1790,
         count: 0,
         amount: 99
       }
@@ -70,7 +72,7 @@ var app = new Vue({
       return sum;
     },
     getBalances: function () {
-      return 100000000000 - this.sumItemPrice;
+      return all_money - this.sumItemPrice;
     }
   }
 });
