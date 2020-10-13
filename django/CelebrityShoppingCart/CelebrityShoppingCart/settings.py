79dc11756707274b52adc11030fe7b494ddb0941
NEXTJun <next0309@gmail.com>
2020-10-13 19:32:10 +0800

    Add docker build files

--- django/CelebrityShoppingCart/CelebrityShoppingCart/settings.py
+++ django/CelebrityShoppingCart/CelebrityShoppingCart/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p#8#hrb!ppqh02bb@8hzrj(^n)$yb4x&eu7w_jp!*e=^l=tmx)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
