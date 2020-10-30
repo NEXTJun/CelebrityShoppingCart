2f75054aed8bbe966b6c09ec7cf5c4c5171963dc
NEXTJun <next0309@gmail.com>
2020-10-30 22:25:56 +0800

    Change project database to PostgreSQL

--- django/CelebrityShoppingCart/CelebrityShoppingCart/settings.py
+++ django/CelebrityShoppingCart/CelebrityShoppingCart/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'CelebrityShoppingCart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'user123',
+        'HOST': 'database',
+        'PORT': '5432',
     }
 }
 
