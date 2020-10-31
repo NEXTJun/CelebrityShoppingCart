ebe21ecbe8b087f583797bdba6e393bd7248c8b4
NEXTJun <next0309@gmail.com>
2020-11-01 00:12:17 +0800

    Add more game function

--- data/static/js/script.js
+++ data/static/js/script.js
@@ -36,6 +36,50 @@ var app = new Vue({
         count: 0,
         amount: 99
       }
+    ],
+    checkout_info_list: [
+      {
+        range_min: 0,
+        range_max: 5000,
+        title: "Gabe Joke",
+        imgUrl: "static/img/Gabe-Joke.jpg",
+        content: "Your purchasing is a joke!",
+      },
+      {
+        range_min: 5000,
+        range_max: 20000,
+        title: "Gabe Good",
+        imgUrl: "static/img/Gabe-Good.jpg",
+        content: "The Summer Sale is upon us!",
+      },
+      {
+        range_min: 20000,
+        range_max: 50000,
+        title: "Gabe Master",
+        imgUrl: "static/img/Gabe-Master.jpg",
+        content: "My child, I return thine wallet bare. Now go forth to your digital playgrounds and frolic.",
+      },
+      {
+        range_min: 50000,
+        range_max: 200000,
+        title: "Gabe King",
+        imgUrl: "static/img/Gabe-King.jpg",
+        content: "In Gaben. We Trust.",
+      },
+      {
+        range_min: 200000,
+        range_max: 500000,
+        title: "Gabe Moses",
+        imgUrl: "static/img/Gabe-Moses.jpg",
+        content: "What comes after 2?",
+      },
+      {
+        range_min: 500000,
+        range_max: all_money,
+        title: "Gabe God",
+        imgUrl: "static/img/Gabe-God.jpg",
+        content: "To our god, G Fat, we hand over our wallets.",
+      },
     ]
   },
   mounted() {
@@ -63,6 +107,10 @@ var app = new Vue({
     },
     getItemList: function () {
     },
+    checkCheckoutInfoInRange: function (item) {
+      var num = this.sumItemPrice;
+      return (num >= item.range_min && num < item.range_max)
+    },
   },
   computed: {
     sumItemPrice: function () {
@@ -74,6 +122,6 @@ var app = new Vue({
     },
     getBalances: function () {
       return all_money - this.sumItemPrice;
-    }
+    },
   }
 });
